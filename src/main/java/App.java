1ac8655141646823d9a93aa74beea622a44d55bb
macBest <khaitboboev94@mail.ru>
2020-11-18 07:55:23 +0300

    Added new Class Cat

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,18 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
         System.out.println(bean.getMessage());
+        System.out.println("####### Cat's Bean ######");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+        System.out.println(catBean2.getName());
+
+        System.out.println(catBean == catBean2);
+
     }
 }
